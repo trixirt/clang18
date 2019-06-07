77df2a6206d2d630224041b47925e0bedee05bb2
Tom Stellard <tstellar@redhat.com>
2019-06-07 22:55:01 +0000

    Pull llvm-test-suite tests from common repo

--- tests/llvm-abi-test-suite/runtest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-
-cp -r /usr/share/llvm-test-suite/ABI-Testsuite .
-cd ABI-Testsuite
-python3 linux-x86.py clang test -v --path /usr/lib64/llvm/ -j 1
--- tests/llvm-test-suite/runtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-
-cmake -G Ninja /usr/share/llvm-test-suite/ -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DTEST_SUITE_LIT_FLAGS="-svj1"
-ninja -j 1 check
