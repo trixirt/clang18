22c49c99fc49a4dab0dfe2aaa477cbd062aa8f57
Jesus Checa Hidalgo <jchecahi@redhat.com>
2022-05-26 18:37:36 +0200

    tests: ppc64le-long-double
    Split the test in two. For Fedora CI we run on x86_64 emulating
    ppc64le using qemu+mock. For RHEL/CentOS run natively.

--- tests/ppc64le-long-double/runtest.sh
+++ tests/ppc64le-long-double/runtest.sh
@@ -1,12 +1,7 @@
 set -e
 
-fedora_release=`rpm -E %{fedora}`
-mock_root=fedora-$fedora_release-ppc64le
-triple=ppc64le-redhat-linux
-
-mock -r $mock_root --isolation=simple --install gcc
-gcc_output=$(mock -r $mock_root --isolation=simple -q --shell gcc -E -dM -x c /dev/null | grep -e __LONG_DOUBLE_IEEE128__ -e __LONG_DOUBLE_IBM128__)
-clang_output=$(clang -target $triple  -E -dM -x c /dev/null | grep -e __LONG_DOUBLE_IEEE128__ -e __LONG_DOUBLE_IBM128__)
+gcc_output=$(gcc -E -dM -x c /dev/null | grep -e __LONG_DOUBLE_IEEE128__ -e __LONG_DOUBLE_IBM128__)
+clang_output=$(clang -E -dM -x c /dev/null | grep -e __LONG_DOUBLE_IEEE128__ -e __LONG_DOUBLE_IBM128__)
 
 echo "gcc:   $gcc_output"
 echo "clang: $clang_output"
